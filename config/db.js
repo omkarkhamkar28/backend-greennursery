2d954b72fc74c9eb9145d7e5befaea22a44d7b9a
Omkar Khamkar <omkarkhamkar2004@gmail.com>
2025-10-03 10:22:06 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const con = async () => {
+    //local database
+    // try {
+    //     let url = `mongodb://${process.env.DB_HOST}:${process.env.MONGOPORT}/${process.env.DB_NAME}`;
+
+    //     await mongoose.connect(url, {
+    //         useNewUrlParser : true,
+    //     });
+    // or) {
+    //     console.error(error.message);
+    //     process.exit();
+    // }
+
+    //Online database
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`connected To Mongodb database ${conn.connection.host}`);
+    } catch (error) {
+        console.error(error.message);
+        console.log("Database connecting error.")
+        process.exit();
+    }
+}
+
+module.exports = con;
