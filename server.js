7dac8d8ed040a6d08d9181b3cac45454635f8755
Omkar Khamkar <omkarkhamkar2004@gmail.com>
2026-01-08 22:02:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,12 +37,12 @@ function main() {
   // app.use("/uploads", express.static(path.join(__dirname, "uploads")));  
   // app.use("/uploadVideos", express.static(path.join(__dirname, "uploadVideos"))); 
 
-  // app.use((req, res, next) => {
-  //   res.setHeader("Access-Control-Allow-Origin", "*");
-  //   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  //   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  //   next();
-  // });
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
 
   app.get("/", (req, res) =>
