75b93c02ddd5883a27184d98595790d924e1377c
Omkar Khamkar <omkarkhamkar2004@gmail.com>
2026-01-08 21:40:01 +0530

    Update uploadProductImage.js

--- middlewares/uploadProductImage.js
+++ middlewares/uploadProductImage.js
@@ -1,1 +1,12 @@
+const multer = require("multer");
 
+const productStorage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./upload"),
+    filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
+});
+const upload = multer({ storage: productStorage });
+
+const profileStorage = multer.memoryStorage();
+const ProductImageUpload = multer({ storage: profileStorage });
+
+module.exports = { upload, ProductImageUpload };
